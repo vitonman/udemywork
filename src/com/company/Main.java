67446056c90e37a199b64a4dec97de1e6f4ad22b
vitodon <45338513+vitonman@users.noreply.github.com>
2019-09-24 15:59:49 +0300

    next exic

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 import com.company.Cylinder.Circle;
 import com.company.Cylinder.Cylinder;
 import com.company.Inherritance_challenge.Car;
+import com.company.Rectangle.Cuboid;
+import com.company.Rectangle.Rectangle;
 import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
 
 import java.util.Scanner;
@@ -14,14 +16,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Circle circle = new Circle(3.75);
-        System.out.println("circle.radius= " + circle.getRadius());
-        System.out.println("circle.area= " + circle.getArea());
-        Cylinder cylinder = new Cylinder(5.55, 7.25);
-        System.out.println("cylinder.radius= " + cylinder.getRadius());
-        System.out.println("cylinder.height= " + cylinder.getHeight());
-        System.out.println("cylinder.area= " + cylinder.getArea());
-        System.out.println("cylinder.volume= " + cylinder.getVolume());
+        Rectangle rectangle = new Rectangle(5, 10);
+        System.out.println("rectangle.width= " + rectangle.getWidth());
+        System.out.println("rectangle.length= " + rectangle.getLength());
+        System.out.println("rectangle.area= " + rectangle.getArea());
+        Cuboid cuboid = new Cuboid(5,10,5);
+        System.out.println("cuboid.width= " + cuboid.getWidth());
+        System.out.println("cuboid.length= " + cuboid.getLength());
+        System.out.println("cuboid.area= " + cuboid.getArea());
+        System.out.println("cuboid.height= " + cuboid.getHeight());
+        System.out.println("cuboid.volume= " + cuboid.getVolume());
+
 
 
 
