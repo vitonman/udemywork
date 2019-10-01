fdffb880692386a8659f474317cf824ad134f3c0
vitodon <45338513+vitonman@users.noreply.github.com>
2019-10-01 12:34:29 +0300

    arraylistChallengeMobileContactList

---
+++ src/com/company/ArrayListChallenge/Contacts.java
@@ -0,0 +1,28 @@
+package com.company.ArrayListChallenge;
+
+public class Contacts {
+
+    private String name;
+    private int number;
+
+    public Contacts(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
--- src/com/company/ArrayListChallenge/MobilePhone.java
+++ src/com/company/ArrayListChallenge/MobilePhone.java
@@ -1,4 +1,28 @@
 package com.company.ArrayListChallenge;
 
+import java.util.ArrayList;
+
 public class MobilePhone {
+
+    private ArrayList<Contacts> contactsList = new ArrayList<Contacts>();
+
+    public void addContact(String name, int number) {
+
+        Contacts contact = new Contacts(name, number);
+        contactsList.add(contact);
+
+    }
+
+    public ArrayList<Contacts> getContactsList() {
+        return contactsList;
+    }
+
+    public void printContactList() {
+        System.out.println("You have " + contactsList.size() + " items in your contact list");
+        for(int i=0; i< contactsList.size(); i++) {
+            System.out.println((i+1) + ". " + contactsList.get(i));
+        }
+    }
+
+
 }
