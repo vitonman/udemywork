4020748735cda9a4ea4d8f7b22bfe1410be2f254
vitodon <45338513+vitonman@users.noreply.github.com>
2019-11-01 04:17:13 +0200

    theatresits

---
+++ src/com/collections/Main.java
@@ -0,0 +1,12 @@
+package com.collections;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Theatre theatre = new Theatre("VitaTeatre", 8, 12);
+        theatre.getSeats();
+
+    }
+
+}
